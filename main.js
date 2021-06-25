3afceba51857151387e4a686c09da4bc8489fd10
tbot-wq <74096322+tbot-wq@users.noreply.github.com>
2021-06-25 23:17:02 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 let audio = document.createElement('audio');
-audio.setAttribute('src','Media/sound.mp3','autoplay');
+audio.setAttribute('src','Media/sound.mp3');
+audio.setAttribute('autoplay')
 audio.loop = true;
 audio.play()
 //Hi
